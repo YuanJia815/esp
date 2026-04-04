c9efa486eb049c9da2c9c5d67240091f24ca4010
YuanJia815 <yuan920815@gmail.com>
2026-04-04 14:34:24 +0800

    test

---
+++ main.js
@@ -0,0 +1,39 @@
+import mqtt from 'mqtt'
+import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+app.use(express.json());
+
+let client = null;
+
+client = mqtt.connect("mqtts://fb65afa1d6c34fa29ba74f059d62716c.s1.eu.hivemq.cloud", {
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  port: 8883,
+});
+client.on("connect", () => {
+  console.log("MQTT connected");
+  client.publish("gate/control", "open");
+});
+client.on("error", (err) => {
+  console.log("MQTT error:", err);
+});
+
+app.get('/on', (req, res) => {
+  client.publish('esp32/control', 'ON');
+  res.send('LED ON');
+});
+
+app.get('/off', (req, res) => {
+  client.publish('esp32/control', 'OFF');
+  res.send('LED OFF');
+});
+
+// Render 需要用 PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
